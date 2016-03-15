bc76ef5859784f88de1d35fa5db9cee46c186c4c
Leonardo <Leonardo>
2016-03-15 15:10:58 -0300

    it's working

--- main.c
+++ main.c
@@ -6,19 +6,15 @@
 #define TMR1L_START_VALUE 0b11011100
 #define TMR1H_START_VALUE 0b00001011
 
-int16 temperatura  = 0,
+long  temperatura  = 0,
       luminosidade = 0,
       umidade      = 0;
       
 #include <string.h>
 #include <registers.h>
 
-unsigned int8 espMode[]            = { "AT+CWMODE=1\r\n\0" },
-              espConnectAp[]       = { "AT+CWJAP=\"xxxx\",\"xxxx\"\r\n\0" },
-              espMultConnections[] = { "AT+CIPMUX=1\r\n\0" },
-              espOpenTCP[]         = { "AT+CIPSTART=4,\"TCP\",\"184.106.153.149\",80\r\n\0" },
-              espPrepareSend[]     = { "AT+CIPSEND=4,71\r\n\0" },
-              espGETcmd[]          = { "GET /update?key=SE27NFVOT83ISQ3Y&field1=%04ld&field2=%04ld&field3=%04ld\r\n\0" };
+unsigned int8 espPrepareSend[]     = { "AT+CIPSEND=4,71\r\n\0" },
+              espGETcmd[]          = { "GET /update?key=SE27NFVOT83ISQ3Y" };
 
 int16 send_data_en = 0, cont = 0;
 
@@ -32,15 +28,19 @@ void USART_send_string(char *s)
 {
         unsigned int16 n, i;
               
-              n = strlen(s);
+        n = strlen(s);
               
         for (i = 0; i <= n; ++i)
                 USART_send_char(s[i]);
 }
 
 void esp8266_config()
-{
-            USART_send_string(espMode);
+{                
+        unsigned int8        espMode[]            = { "AT+CWMODE=1\r\n\0" },
+                              espConnectAp[]       = { "AT+CWJAP=\"xxxx\",\"xxxx\"\r\n\0" },
+                              espMultConnections[] = { "AT+CIPMUX=1\r\n\0" };
+        
+        USART_send_string(espMode);
         delay_ms(500);
      
         USART_send_string(espConnectAp);
@@ -53,32 +53,39 @@ void esp8266_config()
 
 void esp8266_open_tcp()
 {
+         unsigned int8 espOpenTCP[] = { "AT+CIPSTART=4,\"TCP\",\"184.106.153.149\",80\r\n\0" };
+         
         USART_send_string(espOpenTCP);
         delay_ms(5000);
 }
 
 void esp8266_send_data()
 {
-        unsigned int8 tmp[80];
+        unsigned int8 tmp[90];
 
         USART_send_string(espPrepareSend);
         delay_ms(500);
 
-        sprintf(tmp, espGETcmd, temperatura, luminosidade, umidade);
+        sprintf(tmp, "%s&field1=%04ld&field2=%04lds&field3=%04ld\r\n\0", espGETcmd, temperatura, luminosidade, umidade);
         USART_send_string(tmp);
-        delay_ms(500);
+        delay_ms(5000);
+       // USART_send_string(tmp);
+       // delay_ms(5000);
 }
 
 void main()
 {
-        TXSTA  = 0b00000100;  
-        SPBRG  = 129;        
-        RCSTA  = 0b00000000;
-        TRISC  = 0b11111111;
-        RCSTA |= (1 << 7);        // enable SERIAL PORT 
-        TXSTA |= (1 << 5);        // enable TRANSMISSION
+        unsigned int8 op[4] = {"\r\n\0"};
         
-        esp8266_config();
+        TXSTA  = 0b00100100;  
+        SPBRG  = 129;        
+        RCSTA  = 0b10000000;
+        TRISC  = 0b11111111;
+        
+        USART_send_string(op);
+         
+	delay_ms(10);
+	esp8266_config();
         
         T1CON  = 0b00000000;        
         INTCON = 0b11000000; 
@@ -89,17 +96,22 @@ void main()
         T1CON |= 1;
 
         while(true) {
+        	delay_ms(1000);
+        	send_data_en = 1;
+        	
                 if (send_data_en == 1) {
-                         T1CON &= ~1;
-                         
+                        T1CON &= ~1;
+                        
                         esp8266_open_tcp();
                         esp8266_send_data();
                         send_data_en = 0;
                         
-                        TMR1L = TMR1L_START_VALUE;
-                        TMR1H = TMR1H_START_VALUE;
+                        //TMR1L = TMR1L_START_VALUE;
+                        //TMR1H = TMR1H_START_VALUE;
                         T1CON |= 1;
                 }
+                
+                //TXREG = 'x';
         }
 }
 
@@ -107,12 +119,16 @@ void main()
 void isr_timer1()
 {
         cont++;
-
+       // TXREG = 'z';
         if (cont >= 76) {
                 send_data_en = 1;
                 cont = 0;
         }
+       //USART_send_char('x');
+      // TXREG='x';
+       // while (!(TXSTA & 2));
+       // TXREG = '\0';
         
-        TMR1L = TMR1L_START_VALUE;
-        TMR1H = TMR1H_START_VALUE;
+       // TMR1L = TMR1L_START_VALUE;
+       // TMR1H = TMR1H_START_VALUE;
 }
